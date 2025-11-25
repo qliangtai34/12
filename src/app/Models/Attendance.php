113f84c5a5b48f6ce7baf2c2060a43a628129ff4
products <qliangtai34@gmail.com>
2025-11-26 07:54:53 +0900

    サンプルファイルの追加

---
+++ src/app/Models/Attendance.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'date',
+        'clock_in',
+        'clock_out',
+        'status',
+    ];
+
+    /**
+     * Carbon キャスト（必須）
+     */
+    protected $casts = [
+        'date' => 'date',
+        'clock_in' => 'datetime',
+        'clock_out' => 'datetime',
+    ];
+
+    /**
+     * 休憩とのリレーション
+     */
+    public function breaks()
+    {
+        return $this->hasMany(BreakTime::class);
+    }
+}
